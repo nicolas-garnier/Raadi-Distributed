553fc961f4d99c1d622831a722f5d139079e0d5b
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-23 18:40:01 +0100

    [INDEXER][SERVICE] Events updated

--- src/main/java/Raadi/indexer/domain/service/DocumentEvent.java
+++ src/main/java/Raadi/indexer/domain/service/DocumentEvent.java
@@ -4,15 +4,25 @@ import Raadi.entity.DocumentClean;
 import Raadi.indexer.domain.event.DocumentCleanCreated;
 import Raadi.kafkahandler.KProducer;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.lang.reflect.Type;
+
 public class DocumentEvent {
 
+    /**
+     * Subscribe to an event when a DocumentRaw has been published.
+     */
     public void subscribeDocumentRawCreated() {
         // TODO Deserialize DocumentRaw
     }
 
+    /**
+     * Publish an event to Kafka when a DocumentClean has been created.
+     * @param documentClean DocumentClean to publish.
+     */
     public void publishDocumentCleanCreated(DocumentClean documentClean) {
 
         Producer producer = new KProducer("9092").getProducer();
@@ -20,7 +30,8 @@ public class DocumentEvent {
 
         String topicName = "DOCUMENT_CLEAN_CREATED";
         Gson gson = new Gson();
-        String json = gson.toJson(documentCleanCreated);
+        Type type = new TypeToken<DocumentCleanCreated>() {}.getType();
+        String json = gson.toJson(documentCleanCreated, type);
         producer.send(new ProducerRecord<>(topicName, json));
         producer.close();
     }
--- src/main/java/Raadi/indexer/domain/service/QueryEvent.java
+++ src/main/java/Raadi/indexer/domain/service/QueryEvent.java
@@ -4,17 +4,26 @@ import Raadi.entity.TokenData;
 import Raadi.indexer.domain.event.QueryTokenized;
 import Raadi.kafkahandler.KProducer;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.lang.reflect.Type;
 import java.util.HashMap;
 
 public class QueryEvent {
 
+    /**
+     * Subscribe to an event when a Query (string) has been published.
+     */
     public void subscribeTokenizeQuery() {
         // TODO Deserialize Query
     }
 
+    /**
+     * Publish an event to Kafka when a Query (vector) has been tokenized.
+     * @param vector Vector to publish.
+     */
     public void publishQueryTokenized(HashMap<String, TokenData> vector) {
 
         Producer producer = new KProducer("9092").getProducer();
@@ -22,7 +31,8 @@ public class QueryEvent {
 
         String topicName = "QUERY_TOKENIZED";
         Gson gson = new Gson();
-        String json = gson.toJson(queryTokenized);
+        Type type = new TypeToken<QueryTokenized>() {}.getType();
+        String json = gson.toJson(queryTokenized, type);
         producer.send(new ProducerRecord<>(topicName, json));
         producer.close();
     }
