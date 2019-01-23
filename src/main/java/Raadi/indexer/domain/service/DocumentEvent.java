309e69f1b6eeb1992548ed787bd5c40d25ea53b4
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-23 18:02:44 +0100

    [INDEXER][EVENT] Publish function implemented

--- src/main/java/Raadi/indexer/domain/service/DocumentEvent.java
+++ src/main/java/Raadi/indexer/domain/service/DocumentEvent.java
@@ -1,12 +1,27 @@
 package Raadi.indexer.domain.service;
 
+import Raadi.entity.DocumentClean;
+import Raadi.indexer.domain.event.DocumentCleanCreated;
+import Raadi.kafkahandler.KProducer;
+import com.google.gson.Gson;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class DocumentEvent {
 
     public void subscribeDocumentRawCreated() {
         // TODO Deserialize DocumentRaw
     }
 
-    public void publishDocumentCleanCreated() {
-        // TODO Serialize DocumentClean
+    public void publishDocumentCleanCreated(DocumentClean documentClean) {
+
+        Producer producer = new KProducer("9092").getProducer();
+        DocumentCleanCreated documentCleanCreated = new DocumentCleanCreated(documentClean);
+
+        String topicName = "DOCUMENT_CLEAN_CREATED";
+        Gson gson = new Gson();
+        String json = gson.toJson(documentCleanCreated);
+        producer.send(new ProducerRecord<>(topicName, json));
+        producer.close();
     }
 }
--- src/main/java/Raadi/indexer/domain/service/QueryEvent.java
+++ src/main/java/Raadi/indexer/domain/service/QueryEvent.java
@@ -1,12 +1,29 @@
 package Raadi.indexer.domain.service;
 
+import Raadi.entity.TokenData;
+import Raadi.indexer.domain.event.QueryTokenized;
+import Raadi.kafkahandler.KProducer;
+import com.google.gson.Gson;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.HashMap;
+
 public class QueryEvent {
 
     public void subscribeTokenizeQuery() {
         // TODO Deserialize Query
     }
 
-    public void publishQueryTokenized() {
-        // TODO Serialize Vector
+    public void publishQueryTokenized(HashMap<String, TokenData> vector) {
+
+        Producer producer = new KProducer("9092").getProducer();
+        QueryTokenized queryTokenized = new QueryTokenized(vector);
+
+        String topicName = "QUERY_TOKENIZED";
+        Gson gson = new Gson();
+        String json = gson.toJson(queryTokenized);
+        producer.send(new ProducerRecord<>(topicName, json));
+        producer.close();
     }
 }
