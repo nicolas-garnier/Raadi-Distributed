488410ed6421445bdc20c5906bc0d02f237b0a3a
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-23 17:24:06 +0100

    [INDEXER][EVENTS] Implemented

--- src/main/java/Raadi/indexer/domain/event/DocumentCleanCreated.java
+++ src/main/java/Raadi/indexer/domain/event/DocumentCleanCreated.java
@@ -1,4 +1,7 @@
 package Raadi.indexer.domain.event;
 
+import Raadi.entity.DocumentClean;
+
 public class DocumentCleanCreated {
+    private DocumentClean documentClean;
 }
--- src/main/java/Raadi/indexer/domain/event/QueryTokenized.java
+++ src/main/java/Raadi/indexer/domain/event/QueryTokenized.java
@@ -1,4 +1,8 @@
 package Raadi.indexer.domain.event;
 
+import java.util.HashMap;
+import Raadi.entity.TokenData;
+
 public class QueryTokenized {
+    private HashMap<String, TokenData> vector;
 }
