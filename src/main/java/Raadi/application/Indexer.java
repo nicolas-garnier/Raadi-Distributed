b722981d5428c47d792fc14e6f92138d57c689e0
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-24 11:14:46 +0100

    [APPLICATION][INDEXER] Setup main function for build

--- src/main/java/Raadi/application/Indexer.java
+++ src/main/java/Raadi/application/Indexer.java
@@ -1,16 +1,22 @@
 package Raadi.application;
 
+import Raadi.domain.entity.EventStoreEntity;
+import Raadi.domain.repository.EventStoreRepository;
 import Raadi.domain.service.DocumentEventService;
 import Raadi.domain.service.QueryEventService;
 
 
 public class Indexer
 {
-    public static void main( String[] args )
+    @SuppressWarnings("UnnecessaryReturnStatement")
+    public static void main(String[] args )
     {
+        EventStoreEntity eventStoreEntity = new EventStoreEntity();
+        EventStoreRepository eventStoreRepository = new EventStoreRepository(eventStoreEntity);
+
         new Thread(() ->
         {
-            DocumentEventService documentEvent = new DocumentEventService();
+            DocumentEventService documentEvent = new DocumentEventService(eventStoreRepository);
             documentEvent.subscribeDocumentRawCreated();
             return;
         }).start();
